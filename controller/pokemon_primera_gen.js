dab303c479edb64857e4713475e833af0da97b90
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2023-09-27 06:21:40 -0300

    funcionando filtros

--- controller/pokemon_primera_gen.js
+++ controller/pokemon_primera_gen.js
@@ -13,7 +13,7 @@ const getPokemonPorId = (req = request, res = response) => {
                 for (let i in data.types){
                     tipos.push(data.types[i].type.name);
                 }
-                res.json({
+                res.status(200).json({
                     status,
                     statusText,
                     nombre: data.name,
@@ -40,7 +40,7 @@ const getPrimeraGeneracion = (req = request, res = response) =>{
         }
         pokemones.sort((a, b) => Object.keys(a)[0] - Object.keys(b)[0]);
         const nombreGen = data.main_region.name;
-        res.json({
+        res.status(200).json({
             nombreGen,
             pokemones
         })
@@ -50,7 +50,61 @@ const getPrimeraGeneracion = (req = request, res = response) =>{
     })
 }
 
+const getPokemonsPorColorYGen = (req = request, res = response) =>{
+    const {color, gen} = req.query;
+    if (color == undefined){
+        res.status(412).json({error: "Faltan parametros"})
+    } else {
+        axios.get(`https://pokeapi.co/api/v2/pokemon-color/${color}/`)
+            .then(({status, statusText, data}) => {
+            
+                let pokemones = [];
+                for (let i in data.pokemon_species){
+                    let numero = parseInt(data.pokemon_species[i].url.split('/').slice(-2, -1)[0])
+                    let nombre = data.pokemon_species[i].name;
+                    let pokemon = {};
+                    pokemon["nro"] = numero;
+                    pokemon["nombre"] = nombre;
+                    pokemones.push(pokemon);
+                }
+                pokemones.sort((a, b) => a.nro - b.nro);
+
+                if(gen == undefined){
+                    res.status(200).json({
+                        color : data.name,
+                        pokemones
+                    })
+                } else {
+                    const generaciones = {
+                            0 : 0,
+                            1 : 151,
+                            2 : 251,
+                            3 : 386,
+                            4 : 493,
+                            5 : 649,
+                            6 : 721,
+                            7 : 809,
+                            8 : 902,
+                            9 : 1017
+                        };
+                    const pokeGen = pokemones.filter((pokemon) => {
+                        return (pokemon.nro < generaciones[gen]) && (pokemon.nro > generaciones[gen-1])
+                      });
+                    res.status(200).json({
+                        color : data.name,
+                        pokeGen
+                    })
+                }
+            })
+            .catch((error) => {
+                res.status(404).json({error: 'Color no encontrado / no valido'}); 
+            })
+    }
+                
+}
+
 module.exports = {
     getPokemonPorId,
-    getPrimeraGeneracion
+    getPrimeraGeneracion,
+    getPokemonsPorColorYGen
 };
--- routes/pokemon_primera_gen.js
+++ routes/pokemon_primera_gen.js
@@ -1,8 +1,9 @@
 const {Router} = require("express");
-const { getPokemonPorId, getPrimeraGeneracion } = require("../controller/pokemon_primera_gen");
+const { getPokemonPorId, getPrimeraGeneracion, getPokemonsPorColorYGen } = require("../controller/pokemon_primera_gen");
 const rutas = Router();
 
 rutas.get('/pokemon/primer_gen/:id', getPokemonPorId)
 rutas.get('/primera_generacion', getPrimeraGeneracion)
+rutas.get("/pokemones/color", getPokemonsPorColorYGen)
 
 module.exports = rutas;
