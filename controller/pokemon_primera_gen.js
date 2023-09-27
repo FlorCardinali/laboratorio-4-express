824aa37ca01fe7e89686990e06c80aa72d8099a2
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2023-09-27 03:07:31 -0300

    se agregaron controles para errores de id

--- controller/pokemon_primera_gen.js
+++ controller/pokemon_primera_gen.js
@@ -4,23 +4,27 @@ const {request, response} = require("express");
 
 const getPokemonPorId = (req = request, res = response) => {
     const {id} = req.params;
-    
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then(({status, statusText, data}) => {
-        let tipos = [];
-        for (let i in data.types){
-            tipos.push(data.types[i].type.name);
-        }
-        res.json({
-            status,
-            statusText,
-            nombre: data.name,
-            tipos: tipos
-        })
-    })
-    .catch((error) => {
-        res.status(404).json({msg: 'Pokemon no encontrado'}) 
-    })
+    console.log(typeof id);
+    if ( id < 1 || id > 151){
+        res.status(400).json({error:"Parametros incorrectos"});
+    } else {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+            .then(({status, statusText, data}) => {
+                let tipos = [];
+                for (let i in data.types){
+                    tipos.push(data.types[i].type.name);
+                }
+                res.json({
+                    status,
+                    statusText,
+                    nombre: data.name,
+                    tipos: tipos
+                })
+            })
+            .catch((error) => {
+                res.status(404).json({error: 'Pokemon no encontrado'}); 
+            })
+    }
 }
 
 
--- routes/pokemon_primera_gen.js
+++ routes/pokemon_primera_gen.js
@@ -2,6 +2,6 @@ const {Router} = require("express");
 const { getPokemonPorId } = require("../controller/pokemon_primera_gen");
 const rutas = Router();
 
-rutas.get('/pokemon/:id', getPokemonPorId)
+rutas.get('/pokemon/primer_gen/:id', getPokemonPorId)
 
 module.exports = rutas;
